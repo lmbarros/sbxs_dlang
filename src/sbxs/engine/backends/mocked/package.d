14e8308a9cc281b8918be151b6297a384d788276
Leandro Motta Barros <lmb@stackedboxes.org>
2016-04-04 22:56:13 -0300

    Added a dummy test to `engine/raster.d`, to get 100% coverage.
    This is not cheating. The file doesn't have any code. I am just making it
    look safe, as it is. I want my attention to be drawn to real issues, not to
    false negatives like this one.

--- src/sbxs/engine/backends/mocked/package.d
+++ src/sbxs/engine/backends/mocked/package.d
@@ -14,3 +14,12 @@ public import sbxs.engine.backends.mocked.backend;
 public import sbxs.engine.backends.mocked.os;
 public import sbxs.engine.backends.mocked.display;
 public import sbxs.engine.backends.mocked.events;
+
+
+// Just a dummy test, to make this otherwise code-less file appear as 100%
+// covered. I don't want "false negatives" taking my attention instead of real
+// issues.
+unittest
+{
+    assert(true);
+}
